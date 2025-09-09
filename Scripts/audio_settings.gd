6b08e94276317e4d2fc28a86446a852b17397b1a
Jane MacDonald <112093094+DMac-MacDonald@users.noreply.github.com>
2025-09-09 16:06:41 -0400

    fixed audio sliders

--- Scripts/audio_settings.gd
+++ Scripts/audio_settings.gd
@@ -11,10 +11,10 @@ func _on_master_audio_slider_value_changed(value: float) -> void:
 	bus_index = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 func _on_music_audio_slider_value_changed(value: float) -> void:
-	music_audio_label.text="Master: " + str(value * 100) + "%"
-	bus_index = AudioServer.get_bus_index("Sfx")
+	music_audio_label.text="Music: " + str(value * 100) + "%"
+	bus_index = AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 func _on_sfx_audio_slider_value_changed(value: float) -> void:
-	sfx_audio_label.text="Master: " + str(value * 100) + "%"
-	bus_index = AudioServer.get_bus_index("Music")
+	sfx_audio_label.text="Sfx: " + str(value * 100) + "%"
+	bus_index = AudioServer.get_bus_index("Sfx")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
